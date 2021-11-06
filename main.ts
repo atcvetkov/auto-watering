0c1df11e6b81d950af70c67513f67e308c52175c
atcvetkov <63304837+atcvetkov@users.noreply.github.com>
2021-11-06 18:24:43 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,44 @@
-
+function close () {
+    for (let servo1 = 0; servo1 <= 70; servo1++) {
+        let servo12 = 0
+        pins.servoWritePin(AnalogPin.P0, 70 - servo12)
+        basic.pause(20)
+    }
+}
+function open2 () {
+    for (let servo1 = 0; servo1 <= 70; servo1++) {
+        pins.servoWritePin(AnalogPin.P0, servo1)
+        basic.pause(20)
+    }
+}
+let counter = 0
+let limit = 600
+let moisture = pins.analogReadPin(AnalogPin.P1)
+while (true) {
+    moisture = pins.analogReadPin(AnalogPin.P1)
+    led.setBrightness(20)
+    led.plotBarGraph(
+    moisture,
+    1024
+    )
+    if (moisture < limit) {
+        while (counter <= 4 && moisture < limit) {
+            counter += 1
+            open2()
+            close()
+            basic.pause(2000)
+            moisture = pins.analogReadPin(AnalogPin.P1)
+        }
+    } else {
+        counter = 0
+    }
+    if (counter == 5) {
+        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+        basic.showString("KONCHILAS VODA!!!")
+        break;
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.showString("" + moisture)
+    }
+}
